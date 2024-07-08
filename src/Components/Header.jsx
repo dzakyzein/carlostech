cdc8054f94d71d9fa22c6a95b1f7a74b1fbd4be3
Zykiria21 <l200200184@student.ums.ac.id>
2024-07-08 11:51:46 +0700

    text-center header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
       ></div>
       <div className='max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 relative z-10'>
         <div className='mr-auto place-self-center lg:col-span-7 text-center'>
-          <h1 className='text-white text-justify max-w-2xl mb-4 text-4xl font-semibold tracking-tight leading-none md:text-5xl xl:text-6xl'>
+          <h1 className='text-white text-center max-w-2xl mb-4 text-4xl font-semibold tracking-tight leading-none md:text-5xl xl:text-6xl'>
             Carlos Tech - Ahli Close Rasio Mobil, Motor, Alat Berat, dan
             Industri
           </h1>
-          <p className='text-white max-w-2xl mb-6 font-normal text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-justify'>
+          <p className='text-white max-w-2xl mb-6 font-normal text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-center'>
             Kami menyediakan layanan perawatan dan perbaikan terbaik untuk
             motor, mobil, sparepart alat berat, dan sparepart industri dengan
             teknologi manual dengan kepresisian tinggi dan tenaga ahli
