9bb1001ffcc4d3b66d5436cf8368980c03ec67a6
Zykiria21 <l200200184@student.ums.ac.id>
2024-07-23 17:14:07 +0700

    menambahkan readonly pada catatan

--- src/Pages/Adminpage/components/Sales/SaleData.jsx
+++ src/Pages/Adminpage/components/Sales/SaleData.jsx
@@ -363,6 +363,7 @@ const SaleData = () => {
                   type='textarea'
                   id='note'
                   name='note'
+                  readOnly
                   onChange={handleInputChange}
                   defaultValue={selectedSale.note}
                   className='bg-white mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
