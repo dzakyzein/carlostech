5981e9dad344f3b734ea4c12b0f8e71e5277ebcc
Zykiria21 <l200200184@student.ums.ac.id>
2024-10-08 19:55:45 +0700

    mengganti pattern nama di register

--- src/Pages/Signuppage/Register.jsx
+++ src/Pages/Signuppage/Register.jsx
@@ -85,7 +85,7 @@ const Register = () => {
                   className='bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
                   placeholder='John Doe'
                   required
-                  pattern='[A-Z, a-z]'
+                  pattern='[A-Za-z]+'
                 />
               </div>
               <div>
